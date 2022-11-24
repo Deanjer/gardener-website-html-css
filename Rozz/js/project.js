6d3aa5f40ed1358302473da932e87d6ebfa33fd5
Burrito-Princess <willem.lol@outlook.com>
2022-11-24 12:02:06 +0100

    ross commit 6

--- Rozz/js/project.js
+++ Rozz/js/project.js
@@ -28,84 +28,106 @@ function gone1(){
         document.getElementById('number1').style.width = "1000px";
     }
 
-    if (document.getElementById('number1').style.justifyContent == "flex-start"){
-        document.getElementById('number1').style.justifyContent = "center";
-        document.getElementById('number1').style.alignItems = "center";
+    // if (document.getElementById('number1').style.justifyContent == "flex-start"){
+    //     document.getElementById('number1').style.justifyContent = "center";
+    //     document.getElementById('number1').style.alignItems = "center";
+    // }
+    // else{
+    //     document.getElementById('number1').style.justifyContent = "flex-start";
+    //     document.getElementById('number1').style.alignItems = "flex-start";
+    //}
+
+    if (document.getElementById("img1").style.height == "100%"){
+        document.getElementById("img1").style.height = "200%";
+    }else{
+        document.getElementById("img1").style.height = "100%";
     }
-    else{
-        document.getElementById('number1').style.justifyContent = "flex-start";
-        document.getElementById('number1').style.alignItems = "flex-start";
-    };
 
 };
 function gone2(){
     toggleDisplay(document.getElementById("number1"));
     toggleDisplay(document.getElementById("number3"));
     toggleDisplay(document.getElementById("number4"));
-    toggleText(document.getElementById("hidden2"));
+    toggleText(document.getElementById("hidden3"));
+    toggleText(document.getElementById("hidden4"));
 
     if (document.getElementById('number2').style.height == "1000px"){
-        console.log("gone2 done");
         document.getElementById('number2').style.height = "500px";
-    document.getElementById('number2').style.width = "500px";
+        document.getElementById('number2').style.width = "500px";
     }else{
         document.getElementById('number2').style.height = "1000px";
-    document.getElementById('number2').style.width = "1000px";
+        document.getElementById('number2').style.width = "1000px";
     }
-    if (document.getElementById('number2').style.justifyContent == "flex-start"){
-        document.getElementById('number2').style.justifyContent = "center";
-        document.getElementById('number2').style.alignItems = "center";
+    // if (document.getElementById('number2').style.justifyContent == "flex-start"){
+    //     document.getElementById('number2').style.justifyContent = "center";
+    //     document.getElementById('number2').style.alignItems = "center";
+    // }
+    // else{
+    //     document.getElementById('number2').style.justifyContent = "flex-start";
+    //     document.getElementById('number2').style.alignItems = "flex-start";
+    // }
+    if (document.getElementById("img2").style.height == "100%"){
+        document.getElementById("img2").style.height = "200%";
+    }else{
+        document.getElementById("img2").style.height = "100%";
     }
-    else{
-        document.getElementById('number2').style.justifyContent = "flex-start";
-        document.getElementById('number2').style.alignItems = "flex-start";
-    };
 };
 function gone3(){
     toggleDisplay(document.getElementById("number1"));
     toggleDisplay(document.getElementById("number2"));
     toggleDisplay(document.getElementById("number4"));
-    toggleText(document.getElementById("hidden3"));
+    toggleText(document.getElementById("hidden5"));
+    toggleText(document.getElementById("hidden6"));
 
     if (document.getElementById('number3').style.height == "1000px"){
-        console.log("gone3 done");
         document.getElementById('number3').style.height = "500px";
-    document.getElementById('number3').style.width = "500px";
+        document.getElementById('number3').style.width = "500px";
     }else{
         document.getElementById('number3').style.height = "1000px";
-    document.getElementById('number3').style.width = "1000px";
+        document.getElementById('number3').style.width = "1000px";
     }
-    if (document.getElementById('number3').style.justifyContent == "flex-start"){
-        document.getElementById('number3').style.justifyContent = "center";
-        document.getElementById('number3').style.alignItems = "center";
+    // if (document.getElementById('number3').style.justifyContent == "flex-start"){
+    //     document.getElementById('number3').style.justifyContent = "center";
+    //     document.getElementById('number3').style.alignItems = "center";
+    // }
+    // else{
+    //     document.getElementById('number3').style.justifyContent = "flex-start";
+    //     document.getElementById('number3').style.alignItems = "flex-start";
+    // }
+    if (document.getElementById("img3").style.height == "100%"){
+        document.getElementById("img3").style.height = "200%";
+    }else{
+        document.getElementById("img3").style.height = "100%";
     }
-    else{
-        document.getElementById('number3').style.justifyContent = "flex-start";
-        document.getElementById('number3').style.alignItems = "flex-start";
-    };
 };
 function gone4(){
     toggleDisplay(document.getElementById("number1"));
     toggleDisplay(document.getElementById("number2"));
     toggleDisplay(document.getElementById("number3"));
-    toggleText(document.getElementById("hidden4"));
+    toggleText(document.getElementById("hidden7"));
+    toggleText(document.getElementById("hidden8"));
 
     if (document.getElementById('number4').style.height == "1000px"){
-        console.log("gone4 done");
         document.getElementById('number4').style.height = "500px";
-    document.getElementById('number4').style.width = "500px";
+        document.getElementById('number4').style.width = "500px";
     }else{
-    document.getElementById('number4').style.height = "1000px";
-    document.getElementById('number4').style.width = "1000px";
+        document.getElementById('number4').style.height = "1000px";
+        document.getElementById('number4').style.width = "1000px";
     }
-    if (document.getElementById('number4').style.justifyContent == "flex-start"){
-        document.getElementById('number4').style.justifyContent = "center";
-        document.getElementById('number4').style.alignItems = "center";
+    // if (document.getElementById('number4').style.justifyContent == "flex-start"){
+    //     document.getElementById('number4').style.justifyContent = "center";
+    //     document.getElementById('number4').style.alignItems = "center";
+    // }
+    // else{
+    //     document.getElementById('number4').style.justifyContent = "flex-start";
+    //     document.getElementById('number4').style.alignItems = "flex-start";
+    // }
+
+    if (document.getElementById("img4").style.height == "100%"){
+        document.getElementById("img4").style.height = "200%";
+    }else{
+        document.getElementById("img4").style.height = "100%";
     }
-    else{
-        document.getElementById('number4').style.justifyContent = "flex-start";
-        document.getElementById('number4').style.alignItems = "flex-start";
-    };
 };
 
 
