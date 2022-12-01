ef053ad6032c711c3c14b1d91970e60a9dd4171b
Burrito-Princess <willem.lol@outlook.com>
2022-12-01 12:53:13 +0100

    commit 12 ross

---
+++ Rozz/js/project3.js
@@ -0,0 +1,19 @@
+function open1(){
+    console.log("success");
+    window.location.href = "../project1.html";
+}
+
+function open2(){
+    console.log("success");
+    window.location.href = "../project2.html";
+}
+
+function open3(){
+    console.log("success");
+    window.location.href = "../project3.html";
+}
+
+function open4(){
+    console.log("success");
+    window.location.href = "../project4.html";
+}
